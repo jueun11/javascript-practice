295bbcb108cb32f1c2e5eae9e280781dc17738d1
jueun11 <jrangkim11@gmail.com>
2022-12-22 13:39:11 +0900

    자세한것은 노션참고

---
+++ 2022/12/22/parseInt.js
@@ -0,0 +1,11 @@
+let a = "123";
+console.log(typeof(a));
+console.log(typeof(parseInt(a)));
+
+let b = "365일매일매일"
+console.log(typeof(b));
+console.log((parseInt(b)));
+
+let c = "어제는21일오늘은22일"
+console.log(typeof(c));
+console.log((parseInt(c)));
