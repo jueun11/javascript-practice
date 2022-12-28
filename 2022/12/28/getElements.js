d8da54c7dc144642913cc9bb8cc00f90501d79f5
jueun11 <jrangkim11@gmail.com>
2022-12-28 17:41:03 +0900

    class, querySelectorAll

---
+++ 2022/12/28/getElements.js
@@ -0,0 +1,7 @@
+let testClass = document.getElementsByClassName("a");
+console.log(testClass);
+testClass[0].style.backgroundColor = "red";
+let testQuery = document.querySelectorAll('.a');
+// let testQuery = document.querySelector('.a');
+
+console.log(testQuery);
