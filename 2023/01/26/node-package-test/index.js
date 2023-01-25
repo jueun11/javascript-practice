43df331edd388a40aa661d2e704c60efc251f283
jueun11 <99877689+jueun11@users.noreply.github.com>
2023-01-26 05:21:28 +0900

    npm start로 처음 실행되는 파일

---
+++ 2023/01/26/node-package-test/index.js
@@ -0,0 +1,5 @@
+console.log("index 파일 불러옴");
+let randomColor = require("randomcolor"); // import the script
+// *외부 패키지 모듈은 경로가 아니라 그냥 써주면댐
+let color = randomColor(); // a hex code for an attractive color
+console.log(color);
